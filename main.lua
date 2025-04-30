990633f5740a52e4ff0495007142c1027cdd015f
Kyle Twardowski <31957321+AgentWill@users.noreply.github.com>
2025-04-30 09:12:21 -0400

    Upload of created files

---
+++ main.lua
@@ -0,0 +1,98 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Waifu Jokers
+--- MOD_ID: WaifuJokers
+--- PREFIX: waifu
+--- MOD_AUTHOR: [Gibovich, AgentWill]
+--- MOD_DESCRIPTION: Balatro Waifu Jokers Texture Pack, modified by AgentWill to work with Malverk, with art by hosses21 on twitter
+--- VERSION: 1.0.0
+--- DEPENDENCIES: [malverk]
+
+AltTexture{
+	key = 'jokers',
+	set = 'Joker',
+	path = 'Jokers.png',
+	original_sheet = true,
+	keys = {
+		'j_joker',
+		'j_greedy_joker',
+		'j_lusty_joker',
+		'j_wrathful_joker',
+		'j_gluttonous_joker',
+		'j_jolly',
+		'j_zany',
+		'j_mad',
+		'j_crazy',
+		'j_droll',
+		'j_sly',
+		'j_wily',
+		'j_clever',
+		'j_devious',
+		'j_crafty',
+		'j_half',
+		'j_marble',
+		'j_misprint',
+		'j_chaos',
+		'j_steel_joker',
+		'j_gros_michel',
+		'j_even_steven',
+		'j_odd_todd',
+		'j_scholar',
+		'j_blackboard',
+		'j_ice_cream',
+		'j_faceless',
+		'j_green_joker',
+		'j_cavendish',
+		'j_square',
+		'j_hologram',
+		'j_baron',
+		'j_photgraph',
+		'j_fortune_teller',
+		'j_stoner',
+		'j_golden',
+		'j_flash',
+		'j_mr_bones',
+		'j_smeared',
+		'j_throwback',
+		'j_glass',
+		'j_ring_master',
+		'j_blueprint',
+		'j_wee',
+		'j_merry_andy',
+		'j_brainstorm',
+		'j_drivers_license',
+		'j_cartomancer',
+		'j_astronomer',
+		'j_burnt',
+		'j_caino',
+		'j_triboulet',
+		'j_yorick',
+		'j_chicot',
+		'j_perkeo'
+	},
+	soul_keys = {
+		'j_hologram',
+		'j_caino',
+		'j_triboulet',
+		'j_yorick',
+		'j_chicot',
+		'j_perkeo'
+	}
+}
+
+TexturePack{
+	key = 'waifus',
+	textures = {
+		'waifu_jokers'
+	},
+	loc_txt = {
+		name = 'Waifu Jokers',
+		text = {
+			'Retextures Jokers',
+			'to be {C:legendary}Waifus!{}',
+			'Art by {E:1,C:dark_edition,S:1.1}hosses21',
+			'{C:inactive,s:0.8}Code by AgentWill.{}',
+			'Heavily inspired by the',
+			'code of {E:1,C:dark_edition,s:1.1}Victin{}'
+		}
+	}
+}
